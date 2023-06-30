913224baed3168d322d070409a3aded55735c728
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-06-30 12:57:15 +0300

    test Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+import UiButton from "./components/UI/UiButton/UiButton";
+import UiHeart from "./components/UI/UiHeart/UiHeart";
+
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+  <div className="App">
+    <UiButton props />
+    <UiHeart/>
+  </div>);
 }
 
 export default App;
