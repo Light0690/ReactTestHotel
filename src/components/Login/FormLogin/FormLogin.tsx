3dedb06b07bb5a2a661d99b8ece889fb5843c09d
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-06-30 15:58:37 +0300

    prettier

--- src/components/Login/FormLogin/FormLogin.tsx
+++ src/components/Login/FormLogin/FormLogin.tsx
@@ -1,5 +1,5 @@
 import { withFormik, FormikProps, FormikErrors, Form, Field } from "formik";
-import cn from 'classnames';
+import cn from "classnames";
 
 import UiButton from "../../UI/UiButton/UiButton";
 
@@ -21,29 +21,51 @@ const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
     <Form className={styles.form}>
       <h1 className={styles.form__title}>{message}</h1>
       <div className={styles.form__container}>
-        <label className={cn(styles.form__label,touched.email && errors.email && styles.form__errors)} htmlFor="email">
+        <label
+          className={cn(
+            styles.form__label,
+            touched.email && errors.email && styles.form__errors
+          )}
+          htmlFor="email"
+        >
           Логин
         </label>
         <Field
           id="email"
           type="email"
           name="email"
-          className={cn(styles.form__input,touched.email && errors.email && styles.form__errors)}
+          className={cn(
+            styles.form__input,
+            touched.email && errors.email && styles.form__errors
+          )}
         />
-        {touched.email && errors.email && <div className={styles.form__error}>{errors.email}</div>}
+        {touched.email && errors.email && (
+          <div className={styles.form__error}>{errors.email}</div>
+        )}
       </div>
 
       <div className={styles.form__container}>
-        <label className={cn(styles.form__label,touched.password && errors.password && styles.form__errors)} htmlFor="password">
+        <label
+          className={cn(
+            styles.form__label,
+            touched.password && errors.password && styles.form__errors
+          )}
+          htmlFor="password"
+        >
           Пароль
         </label>
         <Field
           id="password"
           type="password"
           name="password"
-          className={cn(styles.form__input,touched.password && errors.password  && styles.form__errors)}
+          className={cn(
+            styles.form__input,
+            touched.password && errors.password && styles.form__errors
+          )}
         />
-        {touched.password && errors.password && <div className={styles.form__error}>{errors.password}</div>}
+        {touched.password && errors.password && (
+          <div className={styles.form__error}>{errors.password}</div>
+        )}
       </div>
 
       <UiButton type="submit" disabled={isSubmitting} />
@@ -68,13 +90,14 @@ const MyForm = withFormik<MyFormProps, FormValues>({
     let errors: FormikErrors<FormValues> = {};
     if (!values.email) {
       errors.email = "*Поле обязательно";
-    } else if(!values.email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/g)) {
+    } else if (!values.email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/g)) {
       errors.email = "*Неверный почтовый адрес";
     }
-     if (!values.password) {
+    if (!values.password) {
       errors.password = "*Поле обязательно";
-    }else if(/[а-яё]/gi.test(values.password) || values.password.length < 8){
-      errors.password = "*Пороль не может содержать кириллицу или быть меньше 8 символов";
+    } else if (/[а-яё]/gi.test(values.password) || values.password.length < 8) {
+      errors.password =
+        "*Пороль не может содержать кириллицу или быть меньше 8 символов";
     }
     return errors;
   },
