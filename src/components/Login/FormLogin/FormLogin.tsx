ad8c9e820192a20270d88bda486c12b0895fc91b
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-01 10:44:55 +0300

    упростил форму
    добавил валидатор yup и полностью перепесал форму ,для краткости

--- src/components/Login/FormLogin/FormLogin.tsx
+++ src/components/Login/FormLogin/FormLogin.tsx
@@ -1,73 +1,46 @@
-import { withFormik, FormikProps, FormikErrors, Form, Field } from "formik";
+import * as Yup from "yup";
+import { Formik, Form, Field } from "formik";
 
 import UiButton from "../../UI/UiButton/UiButton";
 import UiInput from "../../UI/UiInput/UiInput";
 
 import styles from "./FormLogin.module.scss";
 
-interface FormValues {
-  email: string;
-  password: string;
-}
+const SignupSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("*некорректный email")
+    .required("*поле обязательно"),
+  password: Yup.string()
+    .min(8, "*слишком короткий пороль")
+    .required("*поле обязательно"),
+});
 
-interface OtherProps {
-  message: string;
-}
-
-const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
-  const { isSubmitting, message } = props;
-
-  return (
-    <Form className={styles.form}>
-      <h1 className={styles.form__title}>{message}</h1>
-      <Field component={UiInput} type="email" name="email" title="Логин" />
-      <Field
-        component={UiInput}
-        type="password"
-        name="password"
-        title="Пороль"
-      />
-      <UiButton type="submit" disabled={isSubmitting} title={'Войти'}/>
-    </Form>
-  );
-};
-
-interface MyFormProps {
-  message: string;
-}
-
-const MyForm = withFormik<MyFormProps, FormValues>({
-  mapPropsToValues: (props) => {
-    return {
-      email: "",
-      password: "",
-    };
-  },
-
-  validate: (values: FormValues) => {
-    let errors: FormikErrors<FormValues> = {};
-    if (!values.email) {
-      errors.email = "*Поле обязательно";
-    } else if (!values.email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/g)) {
-      errors.email = "*Неверный почтовый адрес";
-    }
-    if (!values.password) {
-      errors.password = "*Поле обязательно";
-    } else if (/[а-яё]/gi.test(values.password) || values.password.length < 8) {
-      errors.password =
-        "*Пороль не может содержать кириллицу или быть меньше 8 символов";
-    }
-    return errors;
-  },
-
-  handleSubmit: (values) => {
-    console.log(values);
-  },
-})(InnerForm);
-
-const FormLogin = () => (
+export const FormLogin = () => (
   <div>
-    <MyForm message="Simple Hotel Check" />
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ errors, touched, isSubmitting }) => (
+        <Form className={styles.form}>
+          <h1 className={styles.form__title}>Signup</h1>
+          <Field component={UiInput} type="email" name="email" title="Логин" />
+          <Field
+            component={UiInput}
+            type="password"
+            name="password"
+            title="Пороль"
+          />
+          <UiButton type="submit" disabled={isSubmitting} title={"Войти"} />
+        </Form>
+      )}
+    </Formik>
   </div>
 );
 
