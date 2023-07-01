63638afe837d09a896ebc82351935f292a12f98c
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-01 11:44:22 +0300

    зарефакторил форму поиска

--- src/components/Main/FormSearch/FormSearch.tsx
+++ src/components/Main/FormSearch/FormSearch.tsx
@@ -1,68 +1,51 @@
-import { withFormik, FormikProps, FormikErrors, Form, Field } from "formik";
+import * as Yup from "yup";
+import { Formik, Form, Field } from "formik";
 
 import UiButton from "../../UI/UiButton/UiButton";
 import UiInput from "../../UI/UiInput/UiInput";
 
 import styles from "./FormSearch.module.scss";
 
-interface FormValues {
-  location: string;
-  Date: string;
-  countDays: number;
-}
+const SignupSchema = Yup.object().shape({
+  location: Yup.string().required("*поле обязательно"),
+  Date: Yup.string().required("*поле обязательно"),
+  countDays : Yup.string().required("*поле обязательно"),
+});
 
-const InnerForm = (props: FormikProps<FormValues>) => {
-  const { isSubmitting } = props;
+export const FormSearch = () => {
+  let date = new Date();
 
-  return (
-    <Form className={styles.form}>
-      <Field component={UiInput} name="location" title="Локация" />
-      <Field
-        component={UiInput}
-        name="Date"
-        type="Date"
-        title="Дата заселения"
-      />
-      <Field component={UiInput} name="countDays" title="Колличество дней" />
-      <UiButton type="submit" disabled={isSubmitting} title={'Найти'}/>
-    </Form>
-  );
-};
-
-interface MyFormProps {
-  initialEmail?: string;
-}
-
-const MyForm = withFormik<MyFormProps, FormValues>({
-  mapPropsToValues: (props) => {
-    let date = new Date();
-
-    return {
-      location: "Москва",
-      Date: `${date.getFullYear()}-0${date.getMonth()}-${date.getDate()}`,
-      countDays: 1,
-    };
-  },
-
-  validate: (values: FormValues) => {
-    let errors: FormikErrors<FormValues> = {};
-
-    if (!values.location) errors.location = "*Поле обязательно";
-    if (!values.Date) errors.Date = "*Поле обязательно";
-    if (!values.countDays) errors.countDays = "*Поле обязательно";
-
-    return errors;
-  },
-
-  handleSubmit: (values) => {
-    console.log(1);
-  },
-})(InnerForm);
-
-const FormSearch = () => {
   return (
     <div>
-      <MyForm />
+      <Formik
+        initialValues={{
+          location: "Москва",
+          Date: `${date.getFullYear()}-0${date.getMonth()}-${date.getDate()}`,
+          countDays: 1,
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className={styles.form}>
+            <Field component={UiInput} name="location" title="Локация" />
+            <Field
+              component={UiInput}
+              name="Date"
+              type="Date"
+              title="Дата заселения"
+            />
+            <Field
+              component={UiInput}
+              name="countDays"
+              title="Колличество дней"
+            />
+            <UiButton type="submit" disabled={isSubmitting} title={"Найти"} />
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
