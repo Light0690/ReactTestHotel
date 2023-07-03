74e7a545bb9ed1277bfba4f8f9fd48dfd875b944
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-03 21:54:56 +0300

    добавил логику с редюсера

--- src/components/Main/FormSearch/FormSearch.tsx
+++ src/components/Main/FormSearch/FormSearch.tsx
@@ -1,47 +1,59 @@
 import * as Yup from "yup";
 import { Formik, Form, Field } from "formik";
+import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
+
+import { setSearchForm } from "../../../redux/slices/mainSlice";
+import { getHotelsAsync } from "../../../redux/sagas/mainSaga";
 
 import UiButton from "../../UI/UiButton/UiButton";
 import UiInput from "../../UI/UiInput/UiInput";
-import UiHeart from "../../UI/UiHeart/UiHeart";
 
 import styles from "./FormSearch.module.scss";
 
 const SignupSchema = Yup.object().shape({
   location: Yup.string().required("*поле обязательно"),
-  Date: Yup.string().required("*поле обязательно"),
+  checkInDate: Yup.string().required("*поле обязательно"),
   countDays: Yup.number()
     .typeError("*поле должно содержать только цифры")
     .required("*поле обязательно"),
 });
 
+interface formValues {
+  location: string;
+  checkInDate: string;
+  countDays: number;
+}
+
 export const FormSearch = () => {
-  let date = new Date();
-  const year = `${date.getFullYear()}`;
-  const month = `${
-    date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()
-  }`;
-  const day = `${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}`;
+  const location = useAppSelector((state) => state.main.location);
+  const checkInDate = useAppSelector((state) => state.main.checkInDate);
+  const countDays = useAppSelector((state) => state.main.countDays);
+  const dispatch = useAppDispatch();
+
+  const submitForm = (values: formValues) => {
+    dispatch(setSearchForm(values));
+    dispatch(getHotelsAsync());
+  };
 
   return (
     <div>
       <Formik
         initialValues={{
-          location: "Москва",
-          Date: `${year}-${month}-${day}`,
-          countDays: 1,
+          location,
+          checkInDate,
+          countDays,
         }}
         validationSchema={SignupSchema}
         onSubmit={(values) => {
-          console.log(values);
+          submitForm(values);
         }}
       >
-        {({ isSubmitting }) => (
+        {({  }) => (
           <Form className={styles.form}>
             <Field component={UiInput} name="location" title="Локация" />
             <Field
               component={UiInput}
-              name="Date"
+              name="checkInDate"
               type="Date"
               title="Дата заселения"
             />
@@ -50,7 +62,7 @@ export const FormSearch = () => {
               name="countDays"
               title="Колличество дней"
             />
-            <UiButton type="submit" disabled={isSubmitting} title={"Найти"} />
+            <UiButton type="submit" disabled={false} title={"Найти"} />
           </Form>
         )}
       </Formik>
