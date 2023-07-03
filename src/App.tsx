6223a280a1833d0fd4b3f72a2b357fea2ca40447
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-03 23:48:56 +0300

    поменял роуты

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ const App: FC = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/main" element={<Main/>} />
+        <Route path="/" element={<Main/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </div>
   );
