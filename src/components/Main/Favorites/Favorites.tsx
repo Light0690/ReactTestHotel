dbe0dc43574893ef7ca4c65e2e3b6fb985531523
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-03 21:59:09 +0300

    добавил логику в компонент

--- src/components/Main/Favorites/Favorites.tsx
+++ src/components/Main/Favorites/Favorites.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 
+import { useAppSelector } from "../../../redux/hooks";
+
 import HotelItem from "../HotelItem/HotelItem";
 import UiSortButton from "../../UI/UiSortButton/UiSortButton";
 
@@ -7,6 +9,8 @@ import styles from "./Favorites.module.scss";
 
 
 const Favorites: FC = () => {
+  const favorites = useAppSelector(state => state.main.favorites)
+
   return (
     <div className={styles.favorites}>
       <h2 className={styles.favorites__title}>Избранное</h2>
@@ -15,8 +19,8 @@ const Favorites: FC = () => {
         <UiSortButton />
       </div>
       <div className={styles.favorites__group}>
-        {[...new Array(3)].map((elem) => (
-          <HotelItem key={Math.random()} />
+        {favorites.map((props) => (
+          <HotelItem key={props.hotelId} {...props} />
         ))}
       </div>
     </div>
