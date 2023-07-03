a39fdcd8ef1a9493a337b20aa769a27e211c421a
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-03 23:51:05 +0300

    добавил авторизацию

--- src/components/Login/FormLogin/FormLogin.tsx
+++ src/components/Login/FormLogin/FormLogin.tsx
@@ -1,10 +1,15 @@
 import * as Yup from "yup";
 import { Formik, Form, Field } from "formik";
+import { Navigate } from "react-router-dom";
+
+import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
+import { setAuth } from "../../../redux/slices/auth";
 
 import UiButton from "../../UI/UiButton/UiButton";
 import UiInput from "../../UI/UiInput/UiInput";
 
 import styles from "./FormLogin.module.scss";
+
 
 const SignupSchema = Yup.object().shape({
   email: Yup.string()
@@ -16,33 +21,47 @@ const SignupSchema = Yup.object().shape({
     .required("*поле обязательно"),
 });
 
-export const FormLogin = () => (
-  <div>
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form className={styles.form}>
-          <h1 className={styles.form__title}>Simple Hotel Check</h1>
-          <Field component={UiInput} type="email" name="email" title="Логин" />
-          <Field
-            component={UiInput}
-            type="password"
-            name="password"
-            title="Пороль"
-          />
-          <UiButton type="submit" disabled={isSubmitting} title={"Войти"} />
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+export const FormLogin = () => {
+  const isAuth = useAppSelector((state) => state.auth.isAuth);
+  const dispatch = useAppDispatch();
+
+  if(isAuth) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values) => {
+          dispatch(setAuth(true));
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className={styles.form}>
+            <h1 className={styles.form__title}>Simple Hotel Check</h1>
+            <Field
+              component={UiInput}
+              type="email"
+              name="email"
+              title="Логин"
+            />
+            <Field
+              component={UiInput}
+              type="password"
+              name="password"
+              title="Пороль"
+            />
+            <UiButton type="submit" disabled={isSubmitting} title={"Войти"} />
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 export default FormLogin;
