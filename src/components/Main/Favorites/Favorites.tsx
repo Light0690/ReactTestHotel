430186be24a8fe595f4aa7a166cc601a0085c822
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-03 11:06:31 +0300

    захардкодит кнопки сорта

--- src/components/Main/Favorites/Favorites.tsx
+++ src/components/Main/Favorites/Favorites.tsx
@@ -1,14 +1,19 @@
 import { FC } from "react";
 
 import HotelItem from "../HotelItem/HotelItem";
+import UiSortButton from "../../UI/UiSortButton/UiSortButton";
 
 import styles from "./Favorites.module.scss";
+
 
 const Favorites: FC = () => {
   return (
     <div className={styles.favorites}>
       <h2 className={styles.favorites__title}>Избранное</h2>
-      <div className={styles.favorites__buttons}>здесь кнопки</div>
+      <div className={styles.favorites__buttons}>
+        <UiSortButton />
+        <UiSortButton />
+      </div>
       <div className={styles.favorites__group}>
         {[...new Array(3)].map((elem) => (
           <HotelItem key={Math.random()} />
