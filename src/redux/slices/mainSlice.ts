546f00a2e338df4a863d9a0497a6ca084e8676e4
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-03 21:51:41 +0300

    добавил главный reducer

---
+++ src/redux/slices/mainSlice.ts
@@ -0,0 +1,75 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+import { dateNext, dateNow } from "../../helpers";
+
+type hotelType = {
+  hotelId: number;
+  hotelName: string;
+  location: any;
+  locationId: number;
+  priceAvg: number;
+  priceFrom: number;
+  pricePercentile: any;
+  stars: number;
+};
+
+interface hotelItem {
+  hotelId: number;
+  hotelName: string;
+  priceAvg: number;
+  stars: number;
+}
+
+interface mainState {
+  location: string;
+  checkInDate: string;
+  checkOutDate: string;
+  countDays: number;
+  hotels: hotelType[];
+  favorites: hotelItem[];
+}
+
+const initialState: mainState = {
+  location: "Москва",
+  checkInDate: dateNow(),
+  checkOutDate: dateNext(dateNow(),1),
+  countDays: 1,
+
+  hotels: [],
+  favorites: [],
+};
+
+const mainSlice = createSlice({
+  name: "main",
+  initialState,
+  reducers: {
+    setSearchForm: (state, actions) => {
+      state.location = actions.payload.location;
+      state.checkInDate = actions.payload.checkInDate;
+      state.countDays = actions.payload.countDays;
+
+      state.checkOutDate = dateNext(
+        actions.payload.checkInDate,
+        actions.payload.countDays
+      );
+    },
+    setHotels: (state, action) => {
+      state.hotels = action.payload;
+    },
+    addFavoritesItem: (state, actions) => {
+      state.favorites = state.favorites.find((elem) => elem.hotelId == actions.payload.hotelId)
+        ? state.favorites.filter((elem) => elem.hotelId != actions.payload.hotelId)
+        : [...state.favorites, actions.payload];
+    },
+  },
+});
+
+export const formData = {
+  location: (state:RootState) => state.main.location,
+  checkInDate : (state: RootState) => state.main.checkInDate,
+  checkOutDate : (state: RootState) => state.main.checkOutDate
+}
+
+export const { setSearchForm, addFavoritesItem, setHotels } = mainSlice.actions;
+export default mainSlice.reducer;
