c0a6e6cc71ec8eaabbd99ccfd7d2ffe20316307a
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-24 17:51:55 +0300

    добавил uiAlert

---
+++ src/components/UI/UiAlert/UiAlert.tsx
@@ -0,0 +1,11 @@
+import styles from "./UiAlert.module.scss";
+
+const UiAlert = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h3>Упсс, что-то пошло не так</h3>
+    </div>
+  );
+};
+
+export default UiAlert;
