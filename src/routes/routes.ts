2f577f6ac891cdad9fa9a676c98bae768262a8ab
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-24 13:50:09 +0300

    выделил routes в отдельный файл

---
+++ src/routes/routes.ts
@@ -0,0 +1,15 @@
+import {
+  HotelsRoute,
+  LoginRoute,
+  NotFoundRoute,
+} from "../constans/routesConst";
+
+import Hotels from "../pages/Hotels";
+import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
+
+export const routes = [
+  { path: HotelsRoute, element: Hotels },
+  { path: LoginRoute, element: Login },
+  { path: NotFoundRoute, element: NotFound }
+];
