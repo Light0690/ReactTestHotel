b2621bf369cbf0fbdc2a8425200fa5033d75f996
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-07-24 13:51:27 +0300

    добавил uiButton

---
+++ src/components/UI/UiButton/UiButton.tsx
@@ -0,0 +1,15 @@
+import React, { MouseEventHandler } from "react";
+
+import styles from "./UiButton.module.scss";
+
+interface props {
+  title: string;
+  onClick: MouseEventHandler<HTMLButtonElement>
+}
+
+const UiButton = ({ title,onClick }: props) => {
+  return( 
+  <button className={styles.button} onClick={onClick}>{title}</button>);
+};
+
+export default UiButton;
