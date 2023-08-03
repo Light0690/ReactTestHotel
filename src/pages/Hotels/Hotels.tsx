82fb49603f6134b83728541737169baa7d0436ee
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-08-03 19:10:34 +0300

    add hook useFetchingWithError

--- src/pages/Hotels/Hotels.tsx
+++ src/pages/Hotels/Hotels.tsx
@@ -2,26 +2,29 @@ import { Navigate } from "react-router-dom";
 import cn from "classnames";
 
 import { useAppSelector } from "@redux/hooks";
+import { useFetchWithError } from "../../hooks/useFetchWithError";
 
 import { addMonthToDate } from "@helpers/date";
+
 
 import Header from "@components/Hotels/Header";
 import FormSearch from "@components/Hotels/FormSearch";
 import Favorites from "@components/Hotels/Favorites";
 import Carousel from "@components/Hotels/Carousel";
 import HotelGroup from "@components/Hotels/HotelGroup";
-import UiAlert from "@components/UI/UiAlert/UiAlert";
 
 import styles from "./Hotels.module.scss";
+
 
 const Hotels = () => {
   const isAuth = useAppSelector((state) => state.auth.isAuth);
   const location = useAppSelector((state) => state.hotels.location);
   const checkInDate = useAppSelector((state) => state.hotels.checkInDate);
   const hotels = useAppSelector((state) => state.hotels.hotels);
+  
   const error = useAppSelector((state) => state.hotels.error);
-console.log(error)
-  // console.log(hotels);
+  const errorTSX = useFetchWithError(error)
+
   if (!isAuth) {
     return <Navigate to={"/login"} />;
   }
@@ -51,7 +54,7 @@ console.log(error)
           <HotelGroup hotels={hotels} />
         </div>
       </div>
-      {error && <UiAlert message={error}/>}
+      {errorTSX}
     </div>
   );
 };
