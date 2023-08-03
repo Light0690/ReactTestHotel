4474852066a53aabf184dddb4e0a1d8862bd1063
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-08-03 19:09:03 +0300

    сделал хук при ошибке запроса

--- src/components/UI/UiAlert/UiAlert.tsx
+++ src/components/UI/UiAlert/UiAlert.tsx
@@ -1,12 +1,15 @@
+import cn from "classnames";
+
 import styles from "./UiAlert.module.scss";
 
 interface Props {
   message: String;
+  isHidden:Boolean
 }
 
-const UiAlert = ({ message }: Props) => {
+const UiAlert = ({ message,isHidden }: Props) => {
   return (
-    <div className={styles.wrapper}>
+    <div className={cn(styles.wrapper,isHidden ? styles.wrapper__hidden : '')}>
       <h3>{message}</h3>
     </div>
   );
