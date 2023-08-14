0b104e273b6b39e17bec913f74467a8a8209163e
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-08-14 15:35:31 +0300

    jsdoc by date

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,4 +1,11 @@
-const formattingDate = (date: Date) => {
+/**
+ * Функция принимает дату и возращает её строкой в формате - 0000-00-00
+ * 
+ * @param { Date } date любая дата  
+ * @returns { string } строка вида 0000-00-00
+ */
+
+const formattingDate = (date: Date): string => {
   const year = date.getFullYear();
   const month = date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth();
   const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
@@ -6,14 +13,27 @@ const formattingDate = (date: Date) => {
   return `${year}-${month}-${day}`;
 };
 
-export const getNowDate = () => {
+
+/**
+ * Функция, которая возвращает нынешнюю дату, отформатированную функцией formattingDate
+ * 
+ * @returns { string } нынешняя дата, отформатированную функцией formattingDate
+ */
+export const getNowDate = (): string => {
   const date = new Date();
   const res = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate());
 
   return formattingDate(res);
 };
 
-export const addMonthToDate = (date: string) => {
+
+/**
+ * Функция, заменяющая номер месяца на его название 
+ * 
+ * @param { string } date отформатированная дата, формата 0000-00-00
+ * @returns { string } отформатированная дата, с заменой цифры месяца на его название 0000-*****-00
+ */
+export const addMonthToDate = (date: string): string => {
   const [year, month, day] = date.split(/-/);
   const months = [
     "января",
