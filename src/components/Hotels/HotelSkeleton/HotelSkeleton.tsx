2d3d8430b4f3960b4e2db3775229e01659a3381b
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-08-21 13:32:27 +0300

    fix css

--- src/components/Hotels/HotelSkeleton/HotelSkeleton.tsx
+++ src/components/Hotels/HotelSkeleton/HotelSkeleton.tsx
@@ -3,18 +3,18 @@ import ContentLoader from "react-content-loader";
 const HotelSkeleton = () => (
   <ContentLoader
     speed={2}
-    width={580}
-    height={90}
-    viewBox="0 0 580 90"
+    width={540}
+    height={86}
+    viewBox="0 0 540 86"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
   >
-    <circle cx="53" cy="45" r="44" />
-    <rect x="122" y="6" rx="10" ry="10" width="194" height="20" />
-    <rect x="122" y="37" rx="10" ry="10" width="120" height="19" />
-    <rect x="122" y="65" rx="10" ry="10" width="89" height="16" />
-    <rect x="538" y="4" rx="10" ry="10" width="30" height="15" />
-    <rect x="487" y="57" rx="10" ry="10" width="89" height="21" />{" "}
+    <rect x="121" y="15" rx="10" ry="10" width="80" height="20" />
+    <rect x="121" y="39" rx="10" ry="10" width="140" height="20" />
+    <rect x="121" y="65" rx="10" ry="10" width="89" height="20" />
+    <rect x="499" y="11" rx="10" ry="10" width="20" height="20" />
+    <rect x="433" y="59" rx="10" ry="10" width="89" height="20" />
+    <circle cx="45" cy="48" r="38" />
   </ContentLoader>
 );
 
