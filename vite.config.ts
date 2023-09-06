182cd8ac95cb3ca3fbf61b107404abf90f07ea45
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-06 19:12:50 +0300

    add baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : '/TestTask_Amwork/',
+  base : '/ReactTestHotelFront/',
   build: {
     chunkSizeWarningLimit:3000,
     rollupOptions: {
