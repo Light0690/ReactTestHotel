5c06b2c9e71d5aa944cecab36acd4070e81b4303
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-07 20:34:42 +0300

    change width skeleton

--- src/components/Hotels/HotelSkeleton/HotelSkeleton.tsx
+++ src/components/Hotels/HotelSkeleton/HotelSkeleton.tsx
@@ -3,18 +3,19 @@ import ContentLoader from "react-content-loader";
 const HotelSkeleton = () => (
   <ContentLoader
     speed={2}
-    width={540}
+    width={740}
     height={86}
-    viewBox="0 0 540 86"
+    viewBox="0 0 740 86"
     backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-  >
+    foregroundColor="#ecebeb">
+
     <rect x="121" y="15" rx="10" ry="10" width="80" height="20" />
     <rect x="121" y="39" rx="10" ry="10" width="140" height="20" />
     <rect x="121" y="65" rx="10" ry="10" width="89" height="20" />
-    <rect x="499" y="11" rx="10" ry="10" width="20" height="20" />
-    <rect x="433" y="59" rx="10" ry="10" width="89" height="20" />
+    <rect x="715" y="14" rx="10" ry="10" width="20" height="20" />
     <circle cx="45" cy="48" r="38" />
+    <rect x="668" y="38" rx="10" ry="10" width="70" height="17" />
+    <rect x="650" y="59" rx="10" ry="10" width="89" height="20" />
   </ContentLoader>
 );
 
