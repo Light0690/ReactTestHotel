9d2601ea01da37a8c65df82f4856875b8ee5aa5c
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-07 20:42:15 +0300

    add hotelById route

--- src/constans/routesConst.ts
+++ src/constans/routesConst.ts
@@ -1,4 +1,5 @@
 export const HotelsRoute = "/";
+export const HotelByIdRoute = "/:id";
 export const LoginRoute = "/login";
 export const FavoriteRoute = "/favorite";
 export const NotFoundRoute = "/*";
