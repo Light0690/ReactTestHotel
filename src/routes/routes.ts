a56d6634cf1c31739dde4c49d375fc4e22cf5013
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-07 17:51:08 +0300

    add favorites lazy route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,17 +1,15 @@
 import { lazy } from "react";
 
-import { HotelsRoute, LoginRoute, NotFoundRoute } from "@constans/routesConst";
-
-// import Hotels from "@pages/Hotels";
-// import Login from "@pages/Login";
-// import NotFound from "@pages/NotFound";
+import { FavoriteRoute, HotelsRoute, LoginRoute, NotFoundRoute } from "@constans/routesConst";
 
 const Hotels = lazy(() => import("@pages/Hotels"));
 const Login = lazy(() => import("@pages/Login"));
+const Favorite = lazy(() => import("@pages/Favorite"));
 const NotFound = lazy(() => import("@pages/NotFound"));
 
 export const routes = [
   { path: HotelsRoute, element: Hotels },
   { path: LoginRoute, element: Login },
+  { path: FavoriteRoute, element: Favorite },
   { path: NotFoundRoute, element: NotFound },
 ];
