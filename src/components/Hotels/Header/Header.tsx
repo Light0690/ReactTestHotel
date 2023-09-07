dda154bc32d395bbce8f0cd825de46eab076c1de
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-07 17:48:37 +0300

    add link to favorite page

--- src/components/Hotels/Header/Header.tsx
+++ src/components/Hotels/Header/Header.tsx
@@ -1,7 +1,11 @@
 import { useAppDispatch } from "@redux/hooks";
+import { Link } from "react-router-dom";
 
 import { setAuth } from "@redux/slices/Auth/authSlice";
 import { useTheme } from "@context/ThemeProvider";
+import { FavoriteRoute } from "@constans/routesConst";
+
+import UiHeart from "@ui/UiHeart";
 
 import { RxExit } from "react-icons/rx";
 import { BsFillSunFill } from "react-icons/bs";
@@ -9,11 +13,12 @@ import { BsMoonFill } from "react-icons/bs";
 
 import styles from "./Header.module.scss";
 
+
 const Header = () => {
   const isTheme = useTheme();
   const dispath = useAppDispatch();
 
-  const onClick = () => {
+  const exit = () => {
     dispath(setAuth(false));
   };
 
@@ -21,10 +26,13 @@ const Header = () => {
     <header className={styles.header}>
       <div className={styles.header__title}>Simple Hotel Check</div>
       <div className={styles.header__buttons}>
+        <Link to={FavoriteRoute} className={styles.header__link}>
+          <UiHeart isActive={true} onClick={() => console.log(1)} />
+        </Link>
         <button className={styles.header__icon} onClick={isTheme.changeIsDark}>
           {isTheme.isDark ? <BsMoonFill /> : <BsFillSunFill />}
         </button>
-        <button className={styles.header__exit} onClick={onClick}>
+        <button className={styles.header__exit} onClick={exit}>
           <span>Выйти</span>
           <RxExit style={{ color: "#41522E" }} />
         </button>
