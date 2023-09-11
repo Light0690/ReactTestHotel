aa6b5a107208cdd22c0bfd60c35e78cf2fe96813
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-11 18:41:50 +0300

    delete login from routesConfig

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,20 +4,17 @@ import {
   FavoriteRoute,
   HotelByIdRoute,
   HotelsRoute,
-  LoginRoute,
   NotFoundRoute,
 } from "@constans/routesConst";
 
 const Hotels = lazy(() => import("@pages/Hotels"));
 const HotelById = lazy(() => import("@pages/HotelById"));
-const Login = lazy(() => import("@pages/Login"));
 const Favorite = lazy(() => import("@pages/Favorite"));
 const NotFound = lazy(() => import("@pages/NotFound"));
 
 export const routes = [
   { path: HotelsRoute, element: Hotels },
   { path: HotelByIdRoute, element: HotelById },
-  { path: LoginRoute, element: Login },
   { path: FavoriteRoute, element: Favorite },
   { path: NotFoundRoute, element: NotFound },
 ];
