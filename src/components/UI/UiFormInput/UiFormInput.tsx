ab2f3443140540b83a79dcfe1e31edcc6a4ccffe
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-11 15:05:26 +0300

    add extends interface

--- src/components/UI/UiFormInput/UiFormInput.tsx
+++ src/components/UI/UiFormInput/UiFormInput.tsx
@@ -1,38 +1,21 @@
-import { ChangeEventHandler, FocusEventHandler } from "react";
+import { InputHTMLAttributes } from "react";
 import cn from "classnames";
 
 import styles from "./UiFormInput.module.scss";
 
-interface Props {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  type: string;
   title: string;
-  onChange: ChangeEventHandler<HTMLInputElement>;
-  onBlur: FocusEventHandler<HTMLInputElement>;
-  value: string | number;
   errors: string | undefined;
   touched: boolean | undefined;
 }
 
-const UiFormInput = ({
-  name,
-  type,
-  title,
-  onChange,
-  onBlur,
-  value,
-  errors,
-  touched,
-}: Props) => {
+const UiFormInput = ({ name, type, title, onChange, onBlur, value, errors, touched }: Props) => {
   return (
     <div className={styles.container}>
       <label
-        className={cn(
-          styles.container__label,
-          touched && errors && styles.container__errors,
-        )}
-        htmlFor={name}
-      >
+        className={cn(styles.container__label, touched && errors && styles.container__errors)}
+        htmlFor={name}>
         {title}
       </label>
       <input
@@ -43,9 +26,7 @@ const UiFormInput = ({
         onChange={onChange}
         onBlur={onBlur}
       />
-      {touched && errors && (
-        <div className={styles.container__error}>{errors}</div>
-      )}
+      {touched && errors && <div className={styles.container__error}>{errors}</div>}
     </div>
   );
 };
