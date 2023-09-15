30ca61aaa6eccbf660ffd44fe78cb9b503a24990
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-15 19:02:43 +0300

    build

---
+++ assets/minoru-4fba79f4.js
@@ -0,0 +1,1 @@
+const t=r=>{if(typeof r!="object"||r===null||r===void 0)throw new Error("invalid params");for(var e in r)if(Object.prototype.hasOwnProperty.call(r,e))return!1;return!0};export{t as i};
