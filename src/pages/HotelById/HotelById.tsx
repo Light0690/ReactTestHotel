323dff7a7f9236019a1c60f567e8a8603106e1a6
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-26 18:46:15 +0300

    add component to page

--- src/pages/HotelById/HotelById.tsx
+++ src/pages/HotelById/HotelById.tsx
@@ -7,7 +7,9 @@ import { hotelByIdSelector } from "@redux/slices/Hotels/hotelsSlice";
 import { fetchHotelById } from "@redux/async/Hotels/fetchHotels";
 
 import HotelInfo from "@components/HotelById/HotelInfo";
+import FormPay from "@components/HotelById/FormPay";
 import Payment from "@components/HotelById/Payment";
+import Advertisement from "@components/HotelById/Advertisement";
 
 import styles from "./HotelById.module.scss";
 
@@ -22,8 +24,6 @@ const HotelById = () => {
       dispatch(fetchHotelById({ id, checkInDate, countDays: +countDays }));
   }, [id, checkInDate, countDays, dispatch]);
 
-  console.log(hotel);
-
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrapper__flex}>
@@ -32,7 +32,11 @@ const HotelById = () => {
           city={hotel?.city || ""}
           days={hotel?.days || ""}
         />
+        <FormPay />
+      </div>
+      <div className={styles.wrapper__flex}>
         <Payment price={hotel?.priceAvg || 0} />
+        <Advertisement />
       </div>
     </div>
   );
