306844b765e89725b6705d546e44e89e03508f2f
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-26 18:54:59 +0300

    build

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: `https://reacttesthotelback-production.up.railway.app/`,
-  baseURL: `http://localhost:4444/`,
+  baseURL: `https://reacttesthotelback-production.up.railway.app/`,
+  // baseURL: `http://localhost:4444/`,
 });
 
 export default instance;
