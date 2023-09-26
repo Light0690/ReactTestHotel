2e5972e409ccc234a2a6d696c864559f6b63df5b
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-26 18:43:17 +0300

    add promocodes and alert functional

--- src/components/HotelById/Payment/Payment.tsx
+++ src/components/HotelById/Payment/Payment.tsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import cn from "classnames";
+
+import { useAppSelector } from "@redux/hooks";
+
+import { bankCardSelector, userSelector } from "@redux/slices/Auth/authSlice";
 
 import UiButton from "@ui/UiButton";
 import UiInput from "@ui/UiInput";
+import UiCheckBox from "@ui/UiCheckBox";
 
 import styles from "./Payment.module.scss";
-import UiCheckBox from "@components/UI/UiCheckBox";
 
 interface Props {
   price: number;
@@ -14,6 +18,17 @@ interface Props {
 const Payment = ({ price }: Props) => {
   const [isChecked, setIsChecked] = useState(false);
   const [promoCode, setPromoCode] = useState("");
+
+  const bankCard = useAppSelector(bankCardSelector);
+  const { email } = useAppSelector(userSelector);
+
+  const priceWithPromo = useMemo(() => {
+    const codes = ["test1", "test2", "test3"];
+
+    return codes.includes(promoCode) ? (price * 0.5).toFixed(0) : price;
+  }, [promoCode, price]);
+
+  const alertText = `Будь это настоящий сайт, то мы сделали бы запрос на оплату\n на сумму - ${priceWithPromo},\n по карте - ${bankCard}\n и подтверждение на почту - ${email}`;
 
   return (
     <div className={cn("block", styles.wrapper)}>
@@ -32,17 +47,26 @@ const Payment = ({ price }: Props) => {
         />
       </div>
       {isChecked && (
-        <UiInput
-          type="text"
-          value={promoCode}
-          onChange={(e) => setPromoCode(e.target.value)}
-          sizeInput="small"
-        />
+        <>
+          <span className={styles.wrapper__price}>
+            промо коды - test1, test2, test3
+          </span>
+          <UiInput
+            type="text"
+            value={promoCode}
+            onChange={(e) => setPromoCode(e.target.value)}
+            sizeInput="small"
+          />
+        </>
       )}
       <hr className={styles.wrapper__hr} />
-      <h4>Итого: {price} ₽</h4>
+      <h4>Итого: {priceWithPromo} ₽</h4>
       <div>
-        <UiButton title="оплатить" size="large" onClick={() => console.log()} />
+        <UiButton
+          title="оплатить"
+          size="large"
+          onClick={() => alert(alertText)}
+        />
       </div>
     </div>
   );
